4d7aa7ea7243c351ea6ebcf8202fbaefccead93d
Curtis Lacy <curtis@engine.co>
2014-06-25 15:59:25 -0400

    Started tying in our javascript on top of drywall's.

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.set('view engine', 'jade');
 app.use(require('morgan')('dev'));
 app.use(require('compression')());
 app.use(require('serve-static')(path.join(__dirname, 'public')));
+app.use(require('serve-static')(path.join(__dirname, 'client-build')));
 app.use(require('body-parser')());
 app.use(require('method-override')());
 app.use(require('cookie-parser')());
