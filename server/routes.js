de481236d1124176533f046ec48874009ec83b76
Curtis Lacy <curtis@engine.co>
2014-07-09 12:20:39 -0400

    Switched connecting an API to be a POST endpoint, and added an endpoint to update API connections to change their connection data.

--- server/routes.js
+++ server/routes.js
@@ -177,7 +177,8 @@ exports = module.exports = function(app, passport) {
   app.get( '/oauth/app/destroy', ensureAuthenticated, require( './oauth/app' ).destroy );
   app.get( '/oauth/app/auth', ensureAuthenticated, require( './oauth/app' ).auth );
   app.get( '/oauth/app/subauth', ensureAuthenticated, require( './oauth/app' ).authRemotes );
-  app.get( '/oauth/api/connect', ensureAuthenticated, require( './oauth/api' ).connect );
+  app.post( '/oauth/api/connect', ensureAuthenticated, require( './oauth/api' ).connect );
+  app.post( '/oauth/api/update', ensureAuthenticated, require( './oauth/api' ).update );
   app.get( '/oauth/api/list', ensureAuthenticated, require( './oauth/api' ).list );
   app.get( '/oauth/api/disconnect', ensureAuthenticated, require( './oauth/api' ).disconnect );
 
