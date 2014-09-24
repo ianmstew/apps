a1b08d01bd13fd7c21e47e0bdb803f8f58720d30
Ian Stewart <ianmstew@gmail.com>
2014-09-23 20:31:37 -0400

    Tutorial module comments!

--- client/app/modules/manager/manager.entities.js
+++ client/app/modules/manager/manager.entities.js
@@ -5,19 +5,27 @@ define(function (require) {
 
   var EditorEntities = Module.extend({
 
+    // Same as this.channel = Backbone.Radio.channel('manager')
     channelName: 'manager',
 
+    // Same as this.channel.reply('apps', this.getApps.bind(this))
+    // See Backbone.Radio documentation
     channelEvents: {
       'apps': ['reply', 'getApps']
     },
 
+    // The shared Apps collection
     apps: null,
 
+    // This module has a single, shared Apps collection, which is why it is only
+    // instantiated here, once.
     initialize: function () {
       this.apps = new AppsCollection();
       notifyUtil.handleModelErrors(this, this.apps);
     },
 
+    // Anyone can ask for the shared Apps collection.
+    // When they do, fetch() is called to ensure the data is up to date.
     getApps: function () {
       this.apps.fetch();
       return this.apps;
--- client/app/modules/manager/manager.module.js
+++ client/app/modules/manager/manager.module.js
@@ -6,23 +6,32 @@ define(function (require) {
 
   var ManagerModule = Module.extend({
 
+    // This module's Radio channel name
     channelName: 'manager',
 
+    // This module's routes
     routes: {
       'apps': 'listApps',
       'apps/create': 'createApp'
     },
 
+    // Module-level entities are provided over events through the entities sub module
     modules: {
       'entities': ManagerEntities
     },
 
+    // Route 'apps'
     listApps: function () {
+
+      // I own a region; show the List layout in it
       var listLayout = new ListLayout();
       this.show(listLayout);
     },
 
+    // Route 'apps/create'
     createApp: function () {
+
+      // I own a region; pass it to the Create presenter, and direct it to "present" immediately
       new CreatePresenter({
         region: this.getRegion(),
         present: true
