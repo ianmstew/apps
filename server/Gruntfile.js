fb91bb418dd2ade1c10795ab369abea501a51a98
Ian Stewart <ianmstew@gmail.com>
2014-12-09 11:04:33 -0500

    Fixed inconsistent background on login page for tall screens

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -74,11 +74,7 @@ module.exports = function(grunt) {
           'public/layouts/**/*.js', '!public/layouts/**/*.min.js',
           'public/views/**/*.js', '!public/views/**/*.min.js'
          ],
-         tasks: ['newer:uglify', 'newer:jshint:client']
-      },
-      serverJS: {
-         files: ['views/**/*.js'],
-         tasks: ['newer:jshint:server']
+         tasks: ['newer:uglify']
       },
       lessViews: {
          files: [
