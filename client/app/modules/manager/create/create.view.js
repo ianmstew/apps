e43ae586b067cc2bff29a522e892333bcbd3ea7e
Becky Sroufe <becky@beckyrasch.com>
2014-12-30 14:31:19 -0500

    Updated Create form with parsley reqs per Settings

--- client/app/modules/manager/create/create.view.js
+++ client/app/modules/manager/create/create.view.js
@@ -1,8 +1,8 @@
 define(function (require) {
   var Marionette = require('marionette');
+  var template = require('hgn!modules/manager/create/create.view');
   var Syphon = require('backbone.syphon');
   var Radio = require('backbone.radio');
-  var template = require('hgn!modules/manager/create/create.view');
   var history = require('lib/util/history');
 
   var CreateView = Marionette.CompositeView.extend({
@@ -10,21 +10,19 @@ define(function (require) {
     className: 'js-main-content',
 
     events: {
-      'submit form': 'formSubmitted'
+      'submit form': 'onSubmit'
     },
 
     onShow: function () {
+      this.ui.form.parsley();
       disableSubmit();
     },
 
-    formSubmitted: function (e) {
-      // Prevents the form from doing a default submit + page refresh
-      e.preventDefault();
-
-      // Gets all the name:value's for the forms elements with a "name"
+    onSubmit: function (evt) {
+      evt.preventDefault();
       var attrs = Syphon.serialize(this);
-      var app = Radio.request('manager', 'new:app', attrs);
-      if (!app.validationError) history.navigate('apps/', { trigger: true });
+      Radio.command('editor', 'update:app', attrs);
+      history.navigate('apps/', { trigger: true });
     },
 
     disableSubmit: function () {
